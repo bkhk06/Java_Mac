e9bb3eb6b31aac633c2e1c8c7287d5e6f5caaf3c
Da.Liu <liuda69@qq.com>
2018-05-23 17:40:15 +0800

    update

--- sklearn_ex1.py
+++ sklearn_ex1.py
@@ -7,6 +7,7 @@ Created on Tue Mar 22  4 09:57:00 2018
 from sklearn.model_selection import  KFold
 
 import mglearn as mg
+import numpy as np
 
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
@@ -28,3 +29,27 @@ pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='
 import matplotlib.pyplot as plt
 iris_dataframe.plot()
 plt.show()
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("X_new.shape: {}".format(X_new.shape))
+prediction = knn.predict(X_new)
+print("Prediction: {}".format(prediction))
+print("Predicted target name: {}".format(
+       iris_dataset['target_names'][prediction]))
+
+y_pred = knn.predict(X_test)
+print("Test set predictions:\n {}".format(y_pred))
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
