b250116c8b507085917ee7cfd0df885d1b77eac1
Da.Liu <liuda69@qq.com>
2018-05-23 17:32:36 +0800

    update

--- sklearn_ex1.py
+++ sklearn_ex1.py
@@ -21,11 +21,10 @@ print("y_train shape: {}".format(y_train.shape))
 
 import pandas as pd
 iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
-print(pd.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
-                           hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mg.cm3))
+pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+                           hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mg.cm3)
+
 
 import matplotlib.pyplot as plt
-
-df = iris_dataframe.cumsum()
-plt.figure(); df.plot();
+iris_dataframe.plot()
 plt.show()
