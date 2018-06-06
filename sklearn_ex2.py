41eedf313cf5ed9017d448f25c728b63ba25dfa4
bkhk06 <bkhk06@sina.com>
2018-06-06 23:56:36 +0800

    update

--- sklearn_ex2.py
+++ sklearn_ex2.py
@@ -27,3 +27,31 @@ print("cancer.keys(): {}".format(cancer.keys()))
 print("Shape of cancer data: {}".format(cancer.data.shape))
 print("Sample counts per class:\n{}".format(
       {n: v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}))
+
+print("Feature names:\n{}".format(cancer.feature_names))
+
+from sklearn.datasets import load_boston
+boston = load_boston()
+print("Date shape: {}".format(boston.data.shape))
+
+X,y = mglearn.datasets.load_extended_boston()
+print("X.shape: {}".format(X.shape))
+
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+plt.show()
+
+from sklearn.model_selection import train_test_split
+X,y = mglearn.datasets.make_forge()
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+print("x_train: {}".format(X_train))
+print("y_train: {}".format(y_train))
+
+print("x_test: {}".format(X_test))
+print("y_test: {}".format(y_test))
+
+from sklearn.neighbors import  KNeighborsClassifier
+clf = KNeighborsClassifier(n_neighbors=3)
+clf.fit(X_train,y_train)
+print("Test set predictions: {}".format(clf.predict(X_test)))
+print("Test set accuracy: {}".format(clf.score(X_test,y_test)))
