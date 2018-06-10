9ce3eb54a08685421f93b31685189fdd760cdb63
bkhk06 <bkhk06@sina.com>
2018-06-10 17:20:47 +0800

    update

--- sklearn_ex2.py
+++ sklearn_ex2.py
@@ -1,5 +1,7 @@
 import mglearn
 import matplotlib.pyplot as plt
+import numpy as np
+
 # generate dataset
 X, y = mglearn.datasets.make_forge()
 # plot dataset
@@ -19,7 +21,6 @@ plt.xlabel("Feature")
 plt.ylabel("Target")
 plt.show()
 
-import numpy as np
 print("Breast cancer:")
 from sklearn.datasets import load_breast_cancer
 cancer = load_breast_cancer()
@@ -69,3 +70,46 @@ for n_neighbors,ax in zip([1,3,9],axes):
       ax.set_ylabel("feature1")
 axes[0].legend(loc=3)
 plt.show()
+
+X_train,X_test,y_train,y_test = train_test_split(cancer.data,cancer.target,stratify=cancer.target,random_state=66)
+training_accurary=[]
+test_accurary=[]
+#try n_neighbors from 1 to 10
+neighbors_settings=range(1,11)
+for n_neighbors in neighbors_settings:
+      #build model
+      clf=KNeighborsClassifier(n_neighbors=n_neighbors)
+      clf.fit(X_train,y_train)
+      #record training set accurary
+      training_accurary.append(clf.score(X_train,y_train))
+      test_accurary.append(clf.score(X_test,y_test))
+
+plt.plot(neighbors_settings,training_accurary,label='training_accurary')
+plt.plot(neighbors_settings,test_accurary,label='test_accurary')
+plt.xlabel('n_neighbors')
+plt.ylabel('Accurary')
+plt.legend()
+plt.show()
+
+mglearn.plots.plot_knn_regression(n_neighbors=1)
+plt.show()
+
+mglearn.plots.plot_knn_regression(n_neighbors=3)
+plt.show()
+
+mglearn.plots.plot_knn_regression(n_neighbors=5)
+plt.show()
+
+from sklearn.neighbors import KNeighborsRegressor
+X,y=mglearn.datasets.make_wave(n_samples=40)
+#split the wave dataset into training and test sets
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
+reg=KNeighborsRegressor(n_neighbors=3)
+reg.fit(X_train,y_train)
+
+print("test set predictions:\n {}".format(reg.predict(X_test)))
+#print('test set predictions:\n {:.1f}'.format(reg.predict(X_test)))
+print("Test set R^2:{:.2f}".format(reg.score(X_test,y_test)))
+
+
+
