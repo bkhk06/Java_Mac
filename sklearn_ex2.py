4eb459434b0618272edb1e78e88df61eeda123a0
bkhk06 <bkhk06@sina.com>
2018-06-12 23:39:24 +0800

    update

--- sklearn_ex2.py
+++ sklearn_ex2.py
@@ -132,3 +132,26 @@ axes[0].legend(["Model predictions","Training data/target",
                 "Test data/target"],loc="best")
 plt.show()
 
+mglearn.plots.plot_linear_regression_wave()
+plt.show()
+
+from sklearn.linear_model import LinearRegression
+X,y = mglearn.datasets.make_wave(n_samples=60)
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=42)
+
+lr = LinearRegression().fit(X_train,y_train)
+print("lr.coef_:%s"% lr.coef_)
+print("lr.intercept_:%s"% lr.intercept_)
+print("training set score_:%f"% lr.score(X_train,y_train))
+print("test set score_:%f"% lr.score(X_test,y_test))
+
+
+from sklearn.linear_model import LinearRegression
+X,y = mglearn.datasets.load_extended_boston()
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+
+lr = LinearRegression().fit(X_train,y_train)
+print("lr.coef_:%s"% lr.coef_)
+print("lr.intercept_:%s"% lr.intercept_)
+print("boston training set score_:%f"% lr.score(X_train,y_train))
+print("boston test set score_:%f"% lr.score(X_test,y_test))
