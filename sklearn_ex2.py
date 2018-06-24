e3e8d9a3aa454957592436f65a58e849d341ae69
bkhk06 <bkhk06@sina.com>
2018-06-24 17:50:54 +0800

    update

--- sklearn_ex2.py
+++ sklearn_ex2.py
@@ -208,3 +208,34 @@ for model,ax in zip([LinearSVC(),LogisticRegression()],axes):
       ax.set_ylabel("Feature 1")
 axes[0].legend()
 plt.show()
+
+mglearn.plots.plot_linear_svc_regularization()
+plt.show()
+
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+X_train,X_test,y_train,y_test = train_test_split(cancer.data,cancer.target,stratify=cancer.target,random_state=42)
+logreg = LogisticRegression().fit(X_train,y_train)
+print("Training set score of Linear LogisticRegression: {:.3f}".format(logreg.score(X_train,y_train)))
+print("Test set score of Linear LogisticRegression: {:.3f}".format(logreg.score(X_test,y_test)))
+
+logreg100 = LogisticRegression(C=100).fit(X_train,y_train)
+print("Training set score of Linear LogisticRegression100: {:.3f}".format(logreg100.score(X_train,y_train)))
+print("Test set score of Linear LogisticRegression100: {:.3f}".format(logreg100.score(X_test,y_test)))
+
+logreg001 = LogisticRegression(C=0.01).fit(X_train,y_train)
+print("Training set score of Linear LogisticRegression001: {:.3f}".format(logreg001.score(X_train,y_train)))
+print("Test set score of Linear LogisticRegression001: {:.3f}".format(logreg001.score(X_test,y_test)))
+
+plt.plot(logreg.coef_.T, 'o',label="C=1")
+plt.plot(logreg100.coef_.T, '^',label="C=100")
+plt.plot(logreg001.coef_.T, 'v',label="C=0.01")
+
+#plt.plot(lr.coef_,'o',label="linearRegression")
+plt.xticks(range(cancer.data.shape[1]),cancer.feature_names,rotation=90)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient  magnitude")
+plt.hlines(0,0,cancer.data.shape[1])
+plt.ylim(-5,5)
+plt.legend()
+plt.show()
