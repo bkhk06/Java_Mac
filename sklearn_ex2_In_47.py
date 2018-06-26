323f42ca2895310bb459bab87db3e855c27fded4
bkhk06 <bkhk06@sina.com>
2018-06-26 13:17:31 +0800

    update

--- sklearn_ex2_In_47.py
+++ sklearn_ex2_In_47.py
@@ -1,6 +1,7 @@
 import mglearn
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
+from sklearn.svm import LinearSVC
 
 X,y = make_blobs(random_state=42)
 mglearn.discrete_scatter(X[:,0],X[:,1],y)
@@ -8,3 +9,7 @@ plt.xlabel("Feature 0")
 plt.ylabel("Feature 1")
 plt.legend(["Class 0","Class 1","Class 2"])
 plt.show()
+
+linear_svm = LinearSVC().fit(X,y)
+print("Coefficient shape: ",linear_svm.coef_.shape)
+print("Intercept shape: ",linear_svm.intercept_.shape)
