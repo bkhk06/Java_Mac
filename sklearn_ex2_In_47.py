3672920e3dcded973762938957fe006fefbe1513
bkhk06 <bkhk06@sina.com>
2018-07-02 20:01:01 +0800

    up

--- sklearn_ex2_In_47.py
+++ sklearn_ex2_In_47.py
@@ -27,3 +27,15 @@ for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_,
     plt.legend(['Class 0', 'Class 1', 'Class 2', 'Line class 0', 'Line class 1',
             'Line class 2'], loc=(1.01, 0.3))
 plt.show()
+
+mglearn.plots.plot_2d_classification(linear_svm, X, fill=True, alpha=.7)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(linear_svm.coef_, linear_svm.intercept_,
+    ['b', 'r', 'g']):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+    plt.legend(['Class 0', 'Class 1', 'Class 2', 'Line class 0', 'Line class 1',
+                'Line class 2'], loc=(1.01, 0.3))
+    plt.xlabel("Feature 0")
+    plt.ylabel("Feature 1")
+plt.show()
