715022146df2f9cecefc0fd41205b85f2207069d
bkhk06 <bkhk06@sina.com>
2019-06-19 18:06:26 +0800

    update

--- selenium_demo1/demo1.py
+++ selenium_demo1/demo1.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome("E:\Selenium\chromedriver.exe")
+#driver = webdriver.Chrome("E:\Selenium\chromedriver.exe")
+driver = webdriver.Chrome()
 driver.get("http://www.baidu.com")
 
 driver.find_element_by_id("kw").clear()
