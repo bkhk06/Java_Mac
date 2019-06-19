816c727ee81d4d218e5797c48a50f39bd4b5c912
bkhk06 <bkhk06@sina.com>
2019-06-19 18:38:45 +0800

    update

--- selenium_demo1/selenium_demo3.py
+++ selenium_demo1/selenium_demo3.py
@@ -4,14 +4,25 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 browser = webdriver.Firefox() # Get local session of firefox
-browser.get("https://cn.bing.com/") # Load page
+#browser.get("https://cn.bing.com/") # Load page
+browser.get("http://www.baidu.com")
 #assert "Yahoo" in browser.title
 print(browser.title)
-elem = browser.find_element_by_name("p") # Find the query box
-elem.send_keys("seleniumhq" + Keys.RETURN)
-time.sleep(0.2) # Let the page load, will be added to the API
+#elem = browser.find_element_by_name("p") # Find the query box
+#elem = browser.find_element_by_id('kw')
+#elem.send_keys("seleniumhq" + Keys.RETURN)
+#time.sleep(0.2) # Let the page load, will be added to the API
+
+browser.find_element_by_id('kw').send_keys('selenium')
+#selenium 元素查找find_element_by_id方法，找到元素后输入信息
+browser.find_element_by_id('su').click()
+time.sleep(10)
+#selenium 元素查找find_element_by_id方法，找到元素后进行点击
+
+'''
 try:
-    browser.find_element_by_xpath("//a[contains(@href,'http://seleniumhq.org')]")
+    browser.find_element_by_xpath("//a[contains(@href,'https://seleniumhq.org')]")
 except NoSuchElementException:
     assert 0, "can't find seleniumhq"
+'''
 browser.close()
